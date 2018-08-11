7127b7ba762f08ac3db91dc8c42eb306e88df821
Dan Anderson <anderson@familymedia.org>
2018-08-10 22:14:43 -0400

    Don't schedule during sync.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('update:index')->everyMinute();
+        // $schedule->command('update:index')->everyMinute();
     }
 
     /**
