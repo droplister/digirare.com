dea08f384cece54cbb3988ee705d23d40e8ab731
Dan Anderson <anderson@familymedia.org>
2018-08-27 01:53:20 -0400

    Update Command

--- app/Console/Commands/Telegram/FileCommand.php
+++ app/Console/Commands/Telegram/FileCommand.php
@@ -27,12 +27,14 @@ class FileCommand extends Command
     {
         // Data
         $card = $this->getCard($arguments);
+        $text = $this->getText($card);
 
         // Send
         $image_url = $this->replyWithImage($card, true);
+        $message = $this->replyWithText($text);
 
         // Track
         $user_id = $this->getUpdate()->getMessage()->getFrom()->getId();
-        $this->outgoingChat($user_id, $image_url, 'file_response');
+        $this->outgoingChat($user_id, $image_url . ' ' . $text, 'file_response');
     }
 }
