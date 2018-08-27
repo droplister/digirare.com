66d61541b9e47e51102782771e8ffc3f1d8b0e72
Dan Anderson <anderson@familymedia.org>
2018-08-27 01:32:51 -0400

    Register Event

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -13,6 +13,9 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
+        'App\Events\CardWasCreated' => [
+            'App\Listeners\CardListener',
+        ],
         'Droplister\XcpCore\App\Events\CreditWasCreated' => [
             'App\Listeners\CollectorListener',
         ],
