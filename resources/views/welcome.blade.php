28b05c1dfec5215b6be8581f07eeb2d71fbda92f
Dan Anderson <anderson@familymedia.org>
2018-08-27 20:44:31 -0400

    Update Welcome Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,45 +45,16 @@
                 position: relative;
             }
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
             .content {
                 text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
             }
         </style>
     </head>
     <body>
         <div class="flex-center position-ref full-height">
             <div class="content">
-                <div class="title m-b-md">
+                <div class="title">
                     <i class="fa fa-diamond"></i> DIGIRARE
-                </div>
-
-                <div class="links">
-                    <a href="https://t.me/digirare">Telegram</a>
-                    <a href="https://xcpdex.com/">XCP DEX</a>
                 </div>
             </div>
         </div>
