596db94ac8a5079712cc0bf53c2254bc02f6f3a9
Dan Anderson <anderson@familymedia.org>
2018-08-28 18:54:06 -0400

    Update Chart Vue

--- resources/assets/js/components/CardOrdersChartComponent.vue
+++ resources/assets/js/components/CardOrdersChartComponent.vue
@@ -30,23 +30,17 @@ export default {
         var buyOrders = {
           name: 'Buy Orders',
           data: data.buy_orders,
-          xAxis: this.$children[0].chart.xAxis.length - 1,
-          yAxis: this.$children[0].chart.yAxis.length - 1
         };
-        this.chartOptions.series.push(buyOrders);
         var sellOrders = {
           name: 'Sell Orders',
           data: data.sell_orders,
-          xAxis: this.$children[0].chart.xAxis.length - 1,
-          yAxis: this.$children[0].chart.yAxis.length - 1
         };
-        this.chartOptions.series.push(sellOrders);
         var orderMatches = {
           name: 'Order Matches',
           data: data.order_matches,
-          xAxis: this.$children[0].chart.xAxis.length - 1,
-          yAxis: this.$children[0].chart.yAxis.length - 1
         };
+        this.chartOptions.series.push(buyOrders);
+        this.chartOptions.series.push(sellOrders);
         this.chartOptions.series.push(orderMatches);
       });
     },
