e944fac6f6dc28b7c4c2013dea0ef788bc9a3162
Dan Anderson <anderson@familymedia.org>
2018-08-28 18:50:57 -0400

    Fix Reference

--- app/Http/Controllers/Api/CardChartsController.php
+++ app/Http/Controllers/Api/CardChartsController.php
@@ -18,14 +18,14 @@ class CardChartsController extends Controller
     public function orderHistory(Request $request, Card $card)
     {
         // Buy Orders
-        $buy_orders = Order::where('backward_asset', '=', $card->xcp_core_asset_name)
+        $buy_orders = Order::where('get_asset', '=', $card->xcp_core_asset_name)
             ->selectRaw('DATE(confirmed_at) as date, COUNT(*) as count')
             ->groupBy('date')
             ->orderBy('date')
             ->get();
 
         // Sell Orders
-        $sell_orders = Order::where('forward_asset', '=', $card->xcp_core_asset_name)
+        $sell_orders = Order::where('give_asset', '=', $card->xcp_core_asset_name)
             ->selectRaw('DATE(confirmed_at) as date, COUNT(*) as count')
             ->groupBy('date')
             ->orderBy('date')
