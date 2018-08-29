e94fff12ef330ac305fc74cb39a3aa167d3664b0
Dan Anderson <anderson@familymedia.org>
2018-08-28 23:55:56 -0400

    Update Index

--- resources/views/cards/partials/index/table.blade.php
+++ resources/views/cards/partials/index/table.blade.php
@@ -4,6 +4,8 @@
             <th scope="col">#</th>
             <th scope="col">Card</th>
             <th scope="col">Holders</th>
+            <th scope="col">Buys</th>
+            <th scope="col">Sells</th>
         </tr>
     </thead>
     <tbody>
@@ -12,6 +14,8 @@
             <th scope="row">{{ $loop->iteration }}</th>
             <td><a href="{{ $card->url }}">{{ $card->name }}</a></td>
             <td>{{ $card->balances_count }}</td>
+            <td>{{ $card->backward_order_matches_count }}</td>
+            <td>{{ $card->forward_order_matches_count }}</td>
         </tr>
         @endforeach
     </tbody>
