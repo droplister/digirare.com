b3daa3fbd18929e62e58aea927422f6beb85df27
Dan Anderson <anderson@familymedia.org>
2018-08-29 02:57:11 -0400

    Look Backwards - Catch Missed Collectors Pre-Card Creation

--- app/Listeners/CardListener.php
+++ app/Listeners/CardListener.php
@@ -17,6 +17,21 @@ class CardListener
      */
     public function handle(CardWasCreated $event)
     {
+        // Card Credits
+        $credits = $event->card->token->credits()->orderBy('tx_index', 'asc')->get();
+
+        // Find Missing
+        foreach($credits as $credit)
+        {
+            // Create Collector
+            Collector::firstOrCreate([
+                'xcp_core_address' => $credit->address,
+            ],[
+                'xcp_core_credit_id' => $credit->id,
+            ]);
+        }
+
+        // Clear Cache
         Cache::forget('cards_array');
     }
 }
