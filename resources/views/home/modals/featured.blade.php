30c90ec67d43092773f93f801b14894d7d0c417d
Dan Anderson <anderson@familymedia.org>
2018-08-31 03:48:18 -0400

    Update Copy

--- resources/views/home/modals/featured.blade.php
+++ resources/views/home/modals/featured.blade.php
@@ -10,10 +10,13 @@
             <div class="modal-body">
                 <p>Anyone can get their card featured. The way it works is simple... it's an auction! The highest four (4) bidders, in this ongoing auction, get the card of their choice displayed on our homepage until they are outbid.</p>
                 <p>We have a special address that we monitor: <br /> <b><a href="https://xchain.io/address/{{ config('digirare.feature_address') }}" target="_blank">{{ config('digirare.feature_address') }}</a></b></p>
-                <p>Send your bid (XCP ONLY) to this address and enter the name of the card you want featured in the memo field. The current high bids are as follows:</p>
+                <p>Send your bid to this address (XCP ONLY) and include the name of the card you want featured in the memo field. Enter just the token name, no other text, or it may not be recognized. No refunds for failed bids!</p>
+                <p>The current high bids are as follows:</p>
+                <p>
                 @foreach($features as $featured)
-                    <p>{{ $loop->iteration }}. {{ $featured->bid_normalized }} XCP</p>
+                    <b>#{{ $loop->iteration }} Spot</b> &nbsp; {{ $featured->bid_normalized }} XCP @if(! $loop->last) <br /> @endif
                 @endforeach
+                </p>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
