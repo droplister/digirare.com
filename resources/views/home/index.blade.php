4a10b496b9e9ead53702f82d1df3731e632be176
Dan Anderson <anderson@familymedia.org>
2018-08-31 03:02:57 -0400

    Update Homepage

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -6,10 +6,9 @@
     <div class="container mt-3">
         <div class="row mb-4">
             <div class="col">
-                <h1 class="display-4 mb-4">
-                    Welcome!
-                </h1>
                 @include('home.partials.index.featured')
+                @include('home.partials.index.cards-chart')
+                @include('home.partials.index.collectors-chart')
             </div>
         </div>
     </div>
