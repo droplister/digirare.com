444f8936662ffa9f758f8f183e7aa0ae8a59f2d8
Dan Anderson <anderson@familymedia.org>
2018-09-05 04:42:29 -0400

    Simple Table

--- resources/views/cards/partials/show/orders.blade.php
+++ resources/views/cards/partials/show/orders.blade.php
@@ -8,7 +8,6 @@
                 <tr>
                     <th scope="col">Price</th>
                     <th scope="col">{{ $card->name }}</th>
-                    <th scope="col">Expires In</th>
                 </tr>
             </thead>
             <tbody>
@@ -16,12 +15,11 @@
                 <tr>
                     <td>{{ $order->trading_price_normalized }} {{ explode('/', $order->trading_pair_normalized)[1] }}</td>
                     <td title="{{ $type === 'Buy' ? $order->get_quantity_normalized : $order->give_quantity_normalized }}">{{ $type === 'Buy' ? number_format($order->get_quantity_normalized) : number_format($order->give_quantity_normalized) }}</td>
-                    <td>{{ \Carbon\Carbon::now()->addMinutes(($order->expire_index - Cache::get('block_index')) * 10)->diffForHumans() }}</td>
                 </tr>
                 @endforeach
                 @if($orders->count() === 0)
                 <tr>
-                    <td colspan="3" class="text-center"><em>No Open {{ $type }} Orders</em></td>
+                    <td colspan="2" class="text-center"><em>No Open {{ $type }} Orders</em></td>
                 </tr>
                 @endif
             </tbody>
