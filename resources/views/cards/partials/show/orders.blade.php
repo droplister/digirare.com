0d57a5b6a946d2bf6ca90ffb85d0c8aa1decc117
Dan Anderson <anderson@familymedia.org>
2018-09-05 03:31:44 -0400

    Update Table

--- resources/views/cards/partials/show/orders.blade.php
+++ resources/views/cards/partials/show/orders.blade.php
@@ -8,22 +8,20 @@
                 <tr>
                     <th scope="col">Price</th>
                     <th scope="col">Amount</th>
-                    <th scope="col">Market</th>
                     <th scope="col">Expires In</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach($orders as $order)
                 <tr>
-                    <td title="{{ $order->trading_price_normalized }} {{ explode('/', $order->trading_pair_normalized)[1] }}">{{ number_format($order->trading_price_normalized) }} {{ explode('/', $order->trading_pair_normalized)[1] }}</td>
-                    <td title="{{ $order->get_quantity_normalized }} {{ $card->name }}">{{ number_format($order->get_quantity_normalized) }} {{ $card->name }}</td>
-                    <td>{{ $order->trading_pair_normalized }}</td>
+                    <td>{{ $order->trading_price_normalized }} {{ explode('/', $order->trading_pair_normalized)[1] }}</td>
+                    <td>{{ $order->get_quantity_normalized }} {{ $card->name }}</td>
                     <td>{{ \Carbon\Carbon::now()->addMinutes(($order->expire_index - Cache::get('block_index')) * 10)->diffForHumans() }}</td>
                 </tr>
                 @endforeach
                 @if($orders->count() === 0)
                 <tr>
-                    <td colspan="5" class="text-center"><em>No Open {{ $type }} Orders</em></td>
+                    <td colspan="3" class="text-center"><em>No Open {{ $type }} Orders</em></td>
                 </tr>
                 @endif
             </tbody>
