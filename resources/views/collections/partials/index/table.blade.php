161e09a60a1976df290942f45f066880ee06b13c
Dan Anderson <anderson@familymedia.org>
2018-09-06 02:34:41 -0400

    Update Table

--- resources/views/collections/partials/index/table.blade.php
+++ resources/views/collections/partials/index/table.blade.php
@@ -7,8 +7,7 @@
                 <th scope="col">Cards</th>
                 <th scope="col">Collectors</th>
                 <th scope="col">Balances</th>
-                <th scope="col" title="# of Unique Cards Collected">Average</th>
-                <th scope="col">Currency</th>
+                <th scope="col" title="Average # of Unique Cards Collected by Collectors">Average</th>
                 <th scope="col">Announced</th>
             </tr>
         </thead>
@@ -20,8 +19,7 @@
                 <td>{{ number_format($collection->cards_count) }}</td>
                 <td>{{ number_format($collection->balances->unique('address')->count()) }}</td>
                 <td>{{ number_format($collection->balances_count) }}</td>
-                <td>{{ number_format($collection->balances_count / $collection->balances->unique('address')->count(), 1) }}</td>
-                <td>{{ $collection->currency }}</td>
+                <td>{{ number_format($collection->balances_count / $collection->balances->unique('address')->count(), 1) }} cards</td>
                 <td>{{ $collection->launched_at->toDateString() }}</td>
             </tr>
             @endforeach
