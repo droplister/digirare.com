8735f1d007779121ab4c12b4ad70b900df85474f
Dan Anderson <anderson@familymedia.org>
2018-09-06 11:06:47 -0400

    Fix Coloring

--- resources/views/orders/partials/table.blade.php
+++ resources/views/orders/partials/table.blade.php
@@ -11,7 +11,7 @@
         <tbody>
             @foreach($orders as $order)
             <tr>
-                <td class="{{ in_array($order->get_asset, $currencies) ? 'text-warning' : 'text-success' }}">{{ in_array($order->get_asset, $currencies) ? 'Selling' : 'Buying' }}</td>
+                <td class="{{ in_array($order->get_asset, $currencies) ? 'text-danger' : 'text-success' }}">{{ in_array($order->get_asset, $currencies) ? 'Selling' : 'Buying' }}</td>
                 <td>{{ in_array($order->get_asset, $currencies) ? $order->give_quantity_normalized : $order->get_quantity_normalized }} {{ in_array($order->get_asset, $currencies) ? $order->give_asset : $order->get_asset }}</td>
                 <td>{{ $order->trading_price_normalized }} {{ explode('/', $order->trading_pair_normalized)[1] }}</td>
                 <td>{{ in_array($order->get_asset, $currencies) ? number_format($order->give_quantity_normalized * $order->trading_price_normalized, 8) : number_format($order->get_quantity_normalized * $order->trading_price_normalized, 8) }} {{ explode('/', $order->trading_pair_normalized)[1] }}</td>
