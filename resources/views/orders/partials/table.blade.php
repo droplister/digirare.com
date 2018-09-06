9d63161d6804315a6d9a786ecc72683845a71d73
Dan Anderson <anderson@familymedia.org>
2018-09-06 14:00:44 -0400

    Update Source

--- resources/views/orders/partials/table.blade.php
+++ resources/views/orders/partials/table.blade.php
@@ -6,6 +6,7 @@
                 <th scope="col">Quantity</th>
                 <th scope="col">Price</th>
                 <th scope="col">Total</th>
+                <th scope="col">Source</th>
                 <th scope="col">Expired In</th>
             </tr>
         </thead>
@@ -16,6 +17,7 @@
                 <td>{{ in_array($order->get_asset, $currencies) ? number_format($order->give_quantity_normalized, 8) : number_format($order->get_quantity_normalized, 8) }} <a href="{{ route('cards.show', ['card' => in_array($order->get_asset, $currencies) ? $order->give_asset : $order->get_asset]) }}">{{ in_array($order->get_asset, $currencies) ? $order->give_asset : $order->get_asset }}</a></td>
                 <td>{{ number_format($order->trading_price_normalized, 8) }} {{ explode('/', $order->trading_pair_normalized)[1] }}</td>
                 <td>{{ in_array($order->get_asset, $currencies) ? number_format($order->give_quantity_normalized * $order->trading_price_normalized, 8) : number_format($order->get_quantity_normalized * $order->trading_price_normalized, 8) }} {{ explode('/', $order->trading_pair_normalized)[1] }}</td>
+                <td><a href="{{ route('collectors.show', ['collector' => $order->source]) }}">{{ str_limit($order->source, 8) }}</a></td>
                 <td>{{ $order->expire_index - $block->block_index }} {{ str_plural('block', $order->expire_index - $block->block_index) }}</td>
             </tr>
             @endforeach
