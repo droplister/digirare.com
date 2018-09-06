22cfcff193c9e14273376f85827c7f6fc976d722
Dan Anderson <anderson@familymedia.org>
2018-09-06 11:00:52 -0400

    Update Big Board

--- resources/views/orders/partials/table.blade.php
+++ resources/views/orders/partials/table.blade.php
@@ -2,17 +2,19 @@
     <table class="table mb-0">
         <thead>
             <tr>
-                <th scope="col">Side</th>
-                <th scope="col">Card</th>
+                <th scope="col">Action</th>
+                <th scope="col">Quantity</th>
                 <th scope="col">Price</th>
+                <th scope="col">Total</th>
             </tr>
         </thead>
         <tbody>
             @foreach($orders as $order)
             <tr>
-                <td>{{ in_array($order->get_asset, $currencies) ? 'Selling' : 'Buying' }}</td>
+                <td class="{{ in_array($order->get_asset, $currencies) ? 'text-warning' : 'text-success' }}">{{ in_array($order->get_asset, $currencies) ? 'Selling' : 'Buying' }}</td>
                 <td>{{ in_array($order->get_asset, $currencies) ? $order->give_quantity_normalized : $order->get_quantity_normalized }} {{ in_array($order->get_asset, $currencies) ? $order->give_asset : $order->get_asset }}</td>
                 <td>{{ $order->trading_price_normalized }} {{ explode('/', $order->trading_pair_normalized)[1] }}</td>
+                <td>{{ in_array($order->get_asset, $currencies) ? number_format($order->give_quantity_normalized * $order->trading_price_normalized, 8) : number_format($order->get_quantity_normalized * $order->trading_price_normalized, 8) }} {{ explode('/', $order->trading_pair_normalized)[1] }}</td>
             </tr>
             @endforeach
         </tbody>
