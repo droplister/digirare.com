f0256812b3ec0c042fa262a126445d9f67084e02
Dan Anderson <anderson@familymedia.org>
2018-09-09 19:03:19 -0400

    Update Views

--- resources/views/collectors/show.blade.php
+++ resources/views/collectors/show.blade.php
@@ -7,7 +7,9 @@
         <div class="row mb-4">
             <div class="col">
                 <h1 class="display-4 mb-4">
-                    {{ $collector->slug }}
+                    <small class="text-highlight"><i aria-hidden="true" class="fa fa-hand-grab-o"></i></small>
+                    Collector
+                    <small class="lead">{{ $collector->slug }}</small>
                 </h1>
                 @include('collectors.partials.show.gallery')
             </div>
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -6,8 +6,20 @@
     <div class="container mt-3">
         <div class="row mb-4">
             <div class="col">
+                <h1 class="display-4 mb-4">
+                    <small class="text-highlight"><i aria-hidden="true" class="fa fa-diamond"></i></small>
+                    DIGIRARE
+                </h1>
                 @include('home.partials.index.featured')
+                <h2 class="display-4 mb-4">
+                    <small class="text-highlight"><i aria-hidden="true" class="fa fa-chain"></i></small>
+                    XCP Cards
+                </h2>
                 @include('home.partials.index.cards-chart')
+                <h2 class="display-4 mb-4">
+                    <small class="text-highlight"><i aria-hidden="true" class="fa fa-hand-grab-o"></i></small>
+                    Collectors
+                </h2>
                 @include('home.partials.index.collectors-chart')
             </div>
         </div>
