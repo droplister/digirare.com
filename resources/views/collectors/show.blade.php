b015aeed1cacd4ee3c4796e80520984468ceb1c5
Dan Anderson <anderson@familymedia.org>
2018-09-10 00:21:36 -0400

    Update

--- resources/views/collectors/show.blade.php
+++ resources/views/collectors/show.blade.php
@@ -9,7 +9,7 @@
                 <h1 class="display-4 mb-4">
                     <small class="text-highlight"><i aria-hidden="true" class="fa fa-hand-grab-o"></i></small>
                     Collector
-                    <small class="lead">{{ $collector->slug }}</small>
+                    <small class="lead d-none d-md-inline-block">{{ $collector->slug }}</small>
                 </h1>
                 @include('collectors.partials.show.gallery')
             </div>
