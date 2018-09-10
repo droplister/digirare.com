7cfab8755b793c0d43c79ebba8418139c690f036
Dan Anderson <anderson@familymedia.org>
2018-09-10 10:49:54 -0400

    Update

--- resources/views/cards/partials/index/filter.blade.php
+++ resources/views/cards/partials/index/filter.blade.php
@@ -10,13 +10,13 @@
         </a>
     </li>
     <li class="nav-item">
-        <a class="nav-link{{ $sort === 'oldest' ? ' active' : '' }}" href="{{ route('cards.index', ['sort' => 'oldest']) }}">
-            Oldest
+        <a class="nav-link{{ $sort === 'newest' ? ' active' : '' }}" href="{{ route('cards.index', ['sort' => 'newest']) }}">
+            Newest
         </a>
     </li>
     <li class="nav-item">
-        <a class="nav-link{{ $sort === 'newest' ? ' active' : '' }}" href="{{ route('cards.index', ['sort' => 'newest']) }}">
-            Newest
+        <a class="nav-link{{ $sort === 'oldest' ? ' active' : '' }}" href="{{ route('cards.index', ['sort' => 'oldest']) }}">
+            Oldest
         </a>
     </li>
 </ul>
