c9b0a9f4d10aed904fca7ecccad482eb74230cbd
Dan Anderson <anderson@familymedia.org>
2018-09-10 00:30:28 -0400

    Update

--- resources/views/cards/trades/show.blade.php
+++ resources/views/cards/trades/show.blade.php
@@ -7,7 +7,7 @@
         @include('cards.partials.show.header')
         <ul class="nav nav-tabs border-bottom-0">
             <li class="nav-item">
-                <a class="nav-link" href="{{ $card->url }}">Overview</a>
+                <a class="nav-link" href="{{ $card->url }}">Card</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="{{ route('cards.collectors.index', ['card' => $card->slug]) }}">Collectors</a>
