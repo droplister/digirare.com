eccf5758270a020e3e30fcfac7ee955fdb35be24
Dan Anderson <anderson@familymedia.org>
2018-09-09 21:42:00 -0400

    Update

--- resources/views/orders/partials/table.blade.php
+++ resources/views/orders/partials/table.blade.php
@@ -15,8 +15,18 @@
             <tr>
                 <td class="{{ in_array($order->get_asset, $currencies) ? 'text-danger' : 'text-success' }}">{{ in_array($order->get_asset, $currencies) ? 'Selling' : 'Buying' }}</td>
                 <td>{{ in_array($order->get_asset, $currencies) ? number_format($order->give_remaining_normalized, 8) : number_format($order->get_remaining_normalized, 8) }} <a href="{{ route('cards.show', ['card' => in_array($order->get_asset, $currencies) ? $order->give_asset : $order->get_asset]) }}">{{ in_array($order->get_asset, $currencies) ? $order->give_asset : $order->get_asset }}</a></td>
-                <td>{{ number_format($order->trading_price_normalized, 8) }} {{ explode('/', $order->trading_pair_normalized)[1] }}</td>
-                <td>{{ in_array($order->get_asset, $currencies) ? number_format($order->give_remaining_normalized * $order->trading_price_normalized, 8) : number_format($order->get_remaining_normalized * $order->trading_price_normalized, 8) }} {{ explode('/', $order->trading_pair_normalized)[1] }}</td>
+                <td>
+                    {{ number_format($order->trading_price_normalized, 8) }}
+                    <a href="{{ route('orders.index', ['currency' => explode('/', $order->trading_pair_normalized)[1]]) }}">
+                        {{ explode('/', $order->trading_pair_normalized)[1] }}
+                    </a>
+                </td>
+                <td>
+                    {{ in_array($order->get_asset, $currencies) ? number_format($order->give_remaining_normalized * $order->trading_price_normalized, 8) : number_format($order->get_remaining_normalized * $order->trading_price_normalized, 8) }}
+                    <a href="{{ route('orders.index', ['currency' => explode('/', $order->trading_pair_normalized)[1]]) }}">
+                        {{ explode('/', $order->trading_pair_normalized)[1] }}
+                    </a>
+                </td>
                 <td><a href="{{ route('collectors.show', ['collector' => $order->source]) }}">{{ str_limit($order->source, 8) }}</a></td>
                 <td>{{ $order->expire_index - $block->block_index }} {{ str_plural('block', $order->expire_index - $block->block_index) }}</td>
             </tr>
