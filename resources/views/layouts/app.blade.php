e3e7cca56d838df116a4b4f798115f717c995d40
Dan Anderson <anderson@familymedia.org>
2018-09-10 03:27:56 -0400

    Update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+@if(! empty($_GET))
+    <meta name="robots" content="noindex,follow">
+@endif
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
