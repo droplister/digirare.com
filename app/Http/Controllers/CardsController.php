6600550653b90fc9902065469effc32fb1fb19c0
Dan Anderson <anderson@familymedia.org>
2018-09-09 23:53:55 -0400

    Update

--- app/Http/Controllers/CardsController.php
+++ app/Http/Controllers/CardsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Card;
 use Droplister\XcpCore\App\Block;
+use Droplister\XcpCore\App\Order;
 use Illuminate\Http\Request;
 
 class CardsController extends Controller
@@ -31,15 +32,36 @@ class CardsController extends Controller
     public function show(Request $request, Card $card)
     {
         $token = $card->token;
+
         $last_match = $card->lastMatch();
+
         $likes = $card->likes()->count();
+
         $dislikes = $card->dislikes()->count();
+
         $balances = $card->balances()->paginate(20);
-        $block = Block::latest('block_index')->first();
+        
         $artists = $card->artists()->orderBy('primary', 'desc')->get();
+        
         $collections = $card->collections()->orderBy('primary', 'desc')->get();
-        $buy_orders = $token ? $token->getOrders()->where('status', '=', 'open')->where('expire_index', '>', $block->block_index)->orderBy('expire_index', 'asc')->get()->sortByDesc('trading_price_normalized') : collect([]);
-        $sell_orders = $token ? $token->giveOrders()->where('status', '=', 'open')->where('expire_index', '>', $block->block_index)->orderBy('expire_index', 'asc')->get()->sortBy('trading_price_normalized') : collect([]);
+
+        $block = Block::latest('block_index')->first();
+        
+        $buy_orders = Order::whereIn('get_asset', [$card->xcp_core_asset_name])
+                    ->where('status', '=', 'open')
+                    ->where('expire_index', '>', $block->block_index)
+                    ->orderBy('expire_index', 'asc')
+                    ->get()
+                    ->sortByDesc('trading_price_normalized');
+
+        $sell_orders = Order::whereIn('give_asset', [$card->xcp_core_asset_name])
+                    ->where('get_asset', '=', $request->currency)
+                    ->where('status', '=', 'open')
+                    ->where('expire_index', '>', $block->block_index)
+                    ->orderBy('expire_index', 'asc')
+                    ->get()
+                    ->sortByDesc('trading_price_normalized');
+        
         $order_matches_count = $token ? $token->backwardOrderMatches->merge($token->forwardOrderMatches)->count() : 0;
 
         return view('cards.show', compact('card', 'artists', 'balances', 'buy_orders', 'collections', 'dislikes', 'last_match', 'likes', 'order_matches_count', 'sell_orders', 'token'));
