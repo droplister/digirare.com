ffc89f359522bf6874c1be6f2f28e9e15b26b91e
Dan Anderson <anderson@familymedia.org>
2018-09-09 23:47:28 -0400

    Update

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/';
+    protected $redirectTo = '/orders';
 
     /**
      * Create a new controller instance.
