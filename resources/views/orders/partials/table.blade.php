def5058747008f7bb1bd862f8ebe02c22658b349
Dan Anderson <anderson@familymedia.org>
2018-09-09 22:22:35 -0400

    Update

--- resources/views/orders/partials/table.blade.php
+++ resources/views/orders/partials/table.blade.php
@@ -14,7 +14,12 @@
             @foreach($orders as $order)
             <tr>
                 <td class="{{ in_array($order->get_asset, $currencies) ? 'text-danger' : 'text-success' }}">{{ in_array($order->get_asset, $currencies) ? 'Selling' : 'Buying' }}</td>
-                <td>{{ in_array($order->get_asset, $currencies) ? number_format($order->give_remaining_normalized, 8) : number_format($order->get_remaining_normalized, 8) }} <a href="{{ route('cards.show', ['card' => in_array($order->get_asset, $currencies) ? $order->give_asset : $order->get_asset]) }}">{{ in_array($order->get_asset, $currencies) ? $order->give_asset : $order->get_asset }}</a></td>
+                <td>
+                    {{ in_array($order->get_asset, $currencies) ? number_format($order->give_remaining_normalized, 8) : number_format($order->get_remaining_normalized, 8) }}
+                    <a href="{{ route('orders.index', ['card' => in_array($order->get_asset, $currencies) ? $order->give_asset : $order->get_asset]) }}">
+                        {{ in_array($order->get_asset, $currencies) ? $order->give_asset : $order->get_asset }}
+                    </a>
+                </td>
                 <td>
                     {{ number_format($order->trading_price_normalized, 8) }}
                     <a href="{{ route('orders.index', ['currency' => explode('/', $order->trading_pair_normalized)[1]]) }}">
