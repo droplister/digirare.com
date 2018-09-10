a7e18c96cc9b99aa7d613c644e21694a5aa94f3d
Dan Anderson <anderson@familymedia.org>
2018-09-10 01:58:54 -0400

    Update

--- resources/views/orders/partials/filters.blade.php
+++ resources/views/orders/partials/filters.blade.php
@@ -22,6 +22,6 @@
         </a>
     @endif
 @else
-    Filter <a href="https://counterparty.io/docs/buy_and_sell_assets_on_the_dex/" target="_blank" class="text-muted">Counterparty DEX</a> orders for collectibles.
+    Filter <a href="https://medium.com/@droplister/counterparty-dex-tutorial-b38dcab102e5" target="_blank" class="text-muted">Counterparty DEX</a> orders for collectibles.
 @endif
 </p>
