8c5f62619cf0c2ffc4ba183b7c0a19c296de132c
Dan Anderson <anderson@familymedia.org>
2018-09-09 22:47:10 -0400

    Update Filters

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -15,25 +15,24 @@
                     Filters: 
                     @if($request->has('action'))
                         <a href="{{ route('orders.index', $request->except('action')) }}" type="button" class="btn btn-danger btn-sm ml-3 py-0 px-1">
-                            x {{ ucfirst($request->has('action')) }}
+                            x {{ ucfirst($request->action) }}
                         </a>
                     @endif
                     @if($request->has('collection'))
                         <a href="{{ route('orders.index', $request->except('collection')) }}" type="button" class="btn btn-danger btn-sm ml-3 py-0 px-1">
-                            x {{ ucwords(str_replace('-', ' ', $request->has('collection'))) }}
+                            x {{ ucwords(str_replace('-', ' ', $request->collection)) }}
                         </a>
                     @endif
                     @if($request->has('currency'))
                         <a href="{{ route('orders.index', $request->except('currency')) }}" type="button" class="btn btn-danger btn-sm ml-3 py-0 px-1">
-                            x {{ ucfirst($request->has('currency')) }}
+                            x {{ ucfirst($request->currency) }}
                         </a>
                     @endif
                     @if($request->has('card'))
                         <a href="{{ route('orders.index', $request->except('card')) }}" type="button" class="btn btn-danger btn-sm ml-3 py-0 px-1">
-                            x {{ $request->has('card') }}
+                            x {{ $request->card }}
                         </a>
                     @endif
-
                 </p>
                 @endif
                 <ul class="nav nav-tabs border-bottom-0">
