678d118074f0b41d8bb3f19e6fcaa4afb201f476
Dan Anderson <anderson@familymedia.org>
2018-11-04 00:31:52 -0400

    Update Card Game

--- resources/views/cards/show.blade.php
+++ resources/views/cards/show.blade.php
@@ -2,18 +2,25 @@
 
 @section('title', $card->name)
 
-@section('content')
-    <div class="container mt-3">
-        <div class="row">
-            <div class="col-md-4 mb-4">
-                @include('cards.partials.show.carousel')
-            </div>
-            <div class="col-md-8 mb-4">
-                @include('cards.partials.show.header')
-                <hr />
-                @include('cards.partials.show.analytics')
+@section('jumbotron')
+    <section class="jumbotron">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 mb-4">
+                    @include('cards.partials.show.carousel')
+                </div>
+                <div class="col-md-8 mb-4">
+                    @include('cards.partials.show.header')
+                    <hr />
+                    @include('cards.partials.show.analytics')
+                </div>
             </div>
         </div>
+    </section>
+@endsection
+
+@section('content')
+    <div class="container">
         <h2 class="display-4 mb-4">
             <small class="text-highlight"><i aria-hidden="true" class="fa fa-chain"></i></small>
             {{ __('DEX Orders') }}
