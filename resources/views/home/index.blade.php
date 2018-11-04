3c9f570e8558810b1823cf9e9e7ea2e112db000f
Dan Anderson <anderson@familymedia.org>
2018-11-04 08:53:28 -0500

    Update Homepage

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -112,6 +112,11 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-6">
+                        <iframe width="100%" height="315" src="https://www.youtube.com/embed/0MBEW2NxNJ4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
+                    <div class="col-md-6">
+                        <br class="d-block d-md-none" />
+                        <br class="d-block d-md-none" />
                         <p class="text-muted mb-0">
                             <a href="#">{{ __('How it Works') }}</a>
                         </p>
@@ -121,13 +126,10 @@
                         <p>
                             Similar to how a blockchain is just a chain of blocks, blockchain art is just art on a blockchain. All the artists featured on our website use Counterparty, a protocol for creating tokens on Bitcoin. Artists and crypto creatives have been using Counterparty to create immutable art and memes since 2015.
                         </p>
-                        <p class="mb-5">
+                        <p class="mb-0">
                             <a href="https://medium.com/kaleidoscope-xcp/the-early-evolution-of-art-on-the-blockchain-part-1-d52d1454e34b" class="btn btn-primary my-2 mr-2" target="_blank">Early History</a>
                             <a href="https://www.artnome.com/news/2018/1/14/what-is-cryptoart" class="btn btn-secondary my-2" target="_blank">Learn More</a>
                         </p>
-                    </div>
-                    <div class="col-md-6">
-                        <iframe width="100%" height="315" src="https://www.youtube.com/embed/0MBEW2NxNJ4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
                 </div>
             </div>
@@ -160,6 +162,12 @@
                 </p>
             </div>
             @endforeach
+        </div>
+        <div class="text-center mb-5">
+            <a href="{{ route('cards.index') }}" class="btn btn-primary">
+                <i class="fa fa-search" aria-hidden="true"></i>
+                Browse All CryptoCollectibles
+            </a>
         </div>
     </div>
     @include('modals.featured')
