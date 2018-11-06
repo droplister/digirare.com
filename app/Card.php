b3ac0e8015c198465efb151aabb00541445a5074
Dan Anderson <anderson@familymedia.org>
2018-11-06 01:41:24 -0500

    Update Numbers

--- app/Card.php
+++ app/Card.php
@@ -80,9 +80,9 @@ class Card extends Model
         // Edge Case
         $supply = $this->token ? $this->token->supply_normalized : 0;
 
-        if($supply < 1000) {
+        if($supply < 9999) {
             return number_format($supply);
-        }elseif($supply > 9999 && $supply < 1000000) {
+        }elseif($supply < 1000000) {
             return number_format($supply / 1000, 1) . 'K';
         }elseif($supply < 1000000000) {
             return number_format($supply / 1000000, 1) . 'M';
