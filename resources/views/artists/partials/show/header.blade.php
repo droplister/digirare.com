15b0f0739e1d9fa389a5f307cd4fd02eca20ffbc
Dan Anderson <anderson@familymedia.org>
2018-11-06 01:09:35 -0500

    Tweak Language

--- resources/views/artists/partials/show/header.blade.php
+++ resources/views/artists/partials/show/header.blade.php
@@ -1,7 +1,7 @@
 <div class="row">
     <div class="col-md-6">
         <p class="text-muted mb-0">
-            <a href="{{ route('artists.index') }}">{{ __('Artists') }}</a>
+            <a href="{{ route('artists.index') }}">{{ __('Artist') }}</a>
         </p>
         <h1 class="display-4 mb-4">
             {{ $artist->name }}
@@ -10,24 +10,24 @@
             {{ $artist->content }}
         </p>
         <h2 class="mb-3 d-inline-block mr-4">
-            <small class="d-block lead" style="font-size: 0.75rem;">Assets</small>
+            <small class="d-block lead" style="font-size: 0.75rem;">{{ __('Pieces') }}</small>
             {{ $cards->total() }}
         </h2>
         <h2 class="mb-3 d-inline-block mr-4">
-            <small class="d-block lead" style="font-size: 0.75rem;">Collectors</small>
+            <small class="d-block lead" style="font-size: 0.75rem;">{{ __('Collectors') }}</small>
             {{ $artist->collectors_count }}
         </h2>
         <h2 class="mb-3 d-inline-block mr-4">
-            <small class="d-block lead" style="font-size: 0.75rem;">Total Prints</small>
+            <small class="d-block lead" style="font-size: 0.75rem;">{{ __('Total Prints') }}</small>
             {{ $artist->total_supply }}
         </h2>
         <p class="mb-5">
             <a href="{{ route('cards.index', ['artist' => $artist->slug]) }}" class="btn btn-primary my-2">
-                Filter Assets
+                {{ __('View Gallery') }}
             </a>
             @if(isset($artist->meta['website']))
             <a href="{{ $artist->meta['website'] }}" class="btn btn-secondary my-2 mr-2" target="_blank">
-                Learn More
+                {{ __('Learn More') }}
             </a>
             @endif
         </p>
