bf960f097c66ecc936d50ff70b9d0fad172143bc
Dan Anderson <anderson@familymedia.org>
2018-11-07 00:54:34 -0500

    Update Titles

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', __('Counterparty DEX - Buy & Sell CryptoCollectibles'))
+@section('title', __('Counterparty DEX - Buy & Sell Crypto Collectibles'))
 
 @section('jumbotron')
     @include('orders.partials.above-the-fold')
