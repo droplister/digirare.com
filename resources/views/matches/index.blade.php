165e7bae24c1b813dbf10325063e71edb31c03c4
Dan Anderson <anderson@familymedia.org>
2018-11-08 03:25:19 -0500

    Update Copy

--- resources/views/matches/index.blade.php
+++ resources/views/matches/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', __('Completed Trades - Counterparty DEX'))
+@section('title', __('Trade History - Counterparty DEX'))
 
 @section('jumbotron')
     @include('matches.partials.above-the-fold')
@@ -9,7 +9,7 @@
 @section('content')
     <div class="container">
         <h5 class="mb-5">
-            {{ $matches->total() }} {{ str_plural('Trades', $matches->total()) }}
+            {{ $matches->total() }} {{ str_plural('Trades', $matches->total()) }} Completed
             <small class="d-none d-md-inline-block pull-right text-muted">
                 Learn about the <a href="https://medium.com/@droplister/counterparty-dex-tutorial-b38dcab102e5" target="_blank">Counterparty DEX</a>.
             </small>
