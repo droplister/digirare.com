474b32bdde81f953f14c3973e56ae37b4d31c175
Dan Anderson <anderson@familymedia.org>
2018-11-09 08:16:42 -0500

    Update Buttons

--- resources/views/orders/partials/button.blade.php
+++ resources/views/orders/partials/button.blade.php
@@ -1,14 +1,23 @@
-<a href="{{ Auth::check() ? route('orders.show', $request->all()) : route('register') }}" class="btn btn-primary" onClick="ga('send', 'event', 'export', 'download', 'orders', {{ $orders->total() }})" download>
-    <i aria-hidden="true" class="fa fa-file-excel-o"></i>
-    Export Open Orders
-    <span class="badge ml-1">
-        {{ $orders->total() }} Rows
-    </span>
-</a>
-<div class="text-muted">
-    @guest
+@guest
+    <a href="{{ route('register') }}" class="btn btn-primary" onClick="ga('send', 'event', 'export', 'register', 'orders', {{ $orders->total() }})">
+        <i aria-hidden="true" class="fa fa-file-excel-o"></i>
+        Export Open Orders
+        <span class="badge ml-1">
+            {{ $orders->total() }} Rows
+        </span>
+    </a>
+    <div class="text-muted">
         (Requires Free Registration)
-    @else
+    </div>
+@else
+    <a href="{{ route('orders.show', $request->all()) }}" class="btn btn-primary" onClick="ga('send', 'event', 'export', 'download', 'orders', {{ $orders->total() }})" download>
+        <i aria-hidden="true" class="fa fa-file-excel-o"></i>
+        Export Open Orders
+        <span class="badge ml-1">
+            {{ $orders->total() }} Rows
+        </span>
+    </a>
+    <div class="text-muted">
         (May take time to generate...)
-    @endguest
-</div>
+    </div>
+@endguest
