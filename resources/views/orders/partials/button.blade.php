7235984ec746027b287ffa68f09a7d81e5a2f0c9
Dan Anderson <anderson@familymedia.org>
2018-11-09 06:11:55 -0500

    Update Export

--- resources/views/orders/partials/button.blade.php
+++ resources/views/orders/partials/button.blade.php
@@ -1,10 +1,14 @@
 <a href="{{ Auth::check() ? route('orders.show', $request->all()) : route('register') }}" class="btn btn-primary">
     <i aria-hidden="true" class="fa fa-file-excel-o"></i>
-    Export Trade History
+    Export Open Orders
     <span class="badge ml-1">
         {{ $orders->total() }} Rows
     </span>
 </a>
 <div class="text-muted">
-    NEW! May take time to load...
+    @guest
+        (Requires Free Registration)
+    @else
+        (May take time to generate...)
+    @endguest
 </div>
