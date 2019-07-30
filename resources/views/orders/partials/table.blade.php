68004a9f501eb8cec84637c0d4af23165506bc87
Dan Anderson <anderson@familymedia.org>
2019-07-29 22:15:02 -0400

    Additional Data Attributes

--- resources/views/orders/partials/table.blade.php
+++ resources/views/orders/partials/table.blade.php
@@ -11,11 +11,13 @@
     </thead>
     <tbody>
         @foreach($orders as $order)
-        <tr data-match-action="{{ $order->trading_type === 'Sell' ? 'BUY' : 'SELL' }}"
+        <tr class="order-data"
+            data-match-action="{{ $order->trading_type === 'Sell' ? 'BUY' : 'SELL' }}"
             data-match-quantity="{{ number_format($order->trading_quantity_normalized, 8) }}" 
             data-match-quantity-asset="{{ $order->trading_pair_base_asset }}" 
             data-match-price="{{ number_format($order->trading_price_normalized, 8) }}" 
-            data-match-price-asset="{{ $order->trading_pair_quote_asset }}">
+            data-match-price-asset="{{ $order->trading_pair_quote_asset }}"
+            data-match-blocks-left="{{ $order->blocks_left }}">
             <td class="{{ $order->trading_type === 'Sell' ? 'text-danger' : 'text-success' }}">
                 {{ $order->trading_type === 'Sell' ? __('Selling') : __('Buying') }}
             </td>
