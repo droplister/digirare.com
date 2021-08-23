3855049a0392915849047d5931f5251409ea78bf
Dan Anderson <anderson@familymedia.org>
2021-08-23 14:45:20 -0400

    Update WidgetController.php

--- app/Http/Controllers/Api/WidgetController.php
+++ app/Http/Controllers/Api/WidgetController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Api;
 
-use Cache;
 use App\Card;
 use App\Http\Resources\WidgetResource;
 use Illuminate\Http\Request;
@@ -17,20 +16,9 @@ class WidgetController extends Controller
      */
     public function show(Request $request, $asset)
     {
-        // Validation
-        $request->validate([
-            'asset' => 'required|exists:cards,xcp_core_asset_name',
-        ]);
+        // New Query
+        $card = Card::with('collections')->where('xcp_core_asset_name', $asset)->firstOrFail();
 
-        // Cache Slug
-        $cache_slug = 'asset_' . str_slug(serialize($request->all()));
-
-        // Get Asset
-        return Cache::remember($cache_slug, 60, function () use ($request) {
-            // New Query
-            $card = Card::with('collections')->where('xcp_core_asset_name', $asset)->first();
-
-            return new WidgetResource($card);
-        });
+        return new WidgetResource($card);
     }
 }
