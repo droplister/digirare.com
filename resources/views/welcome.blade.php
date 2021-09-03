5071dc8e90f947bf3d54414d72d748dbd66cb5a3
Dan Anderson <anderson@familymedia.org>
2021-09-03 03:43:19 -0400

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,8 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
     <head>
+        <script defer data-domain="digirare.com" src="https://plausible.io/js/plausible.js"></script>
+
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
