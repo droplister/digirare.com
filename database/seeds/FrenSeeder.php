c96145fd327bc1fb5566a9f187a1579a1c9486d4
Dan Anderson <anderson@familymedia.org>
2021-11-22 02:04:31 -0500

    Create FrenSeeder.php

---
+++ database/seeds/FrenSeeder.php
@@ -0,0 +1,59 @@
+<?php
+
+use App\Collection;
+use App\Traits\ImportsCards;
+use Illuminate\Database\Seeder;
+
+class FrenSeeder extends Seeder
+{
+    use ImportsCards;
+
+    /**
+     * Collection
+     *
+     * @var \App\Collection
+     */
+    protected $collection;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->collection = Collection::firstOrCreate([
+            'name' => 'Punk Frens',
+        ], [
+            'meta' => null,
+            'currency' => 'XCP',
+        ]);
+    }
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        foreach (range(1, 1000) as $number) {
+        	// Fren #
+        	$name = "PUNKFREN.{$number}";
+        	$image_url = "https://d3vy6llg1tejsu.cloudfront.net/images/{$number}.png";
+
+            // The Asset
+            $xcp_core_asset_name = $this->getAssetName($name);
+
+            // Creation
+            $card = $this->firstOrCreateCard($xcp_core_asset_name, $name);
+
+            // Relation
+            $card->collections()->syncWithoutDetaching([
+                $this->collection->id => [
+                    'image_url' => $image_url,
+                ]
+            ]);
+        }
+    }
+}
